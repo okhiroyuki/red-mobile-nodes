26c5815fee7b1ae6632fbd14d7313ed091ed3b88
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2019-03-17 23:11:15 +0900

    add light node

--- nodes/light.js
+++ nodes/light.js
@@ -2,44 +2,42 @@ module.exports = function(RED) {
     'use strcit';
 
     const axios = require('axios');
-    const BASE_URL = 'http://localhost';
+    const BASE_URL = 'http://127.0.0.1';
     const PATH =  '/light';
 
-    function AndroidLightNode(n) {
+    function RedMobileLightNode(n) {
         RED.nodes.createNode(this, n);
         let node = this;
 
         node.on('input', function(msg) {
             let config = {
-                baseURL: BASE_URL + "/" + RED.settings.port,
+                baseURL: BASE_URL + ":" + RED.settings.redMobilePort,
                 url: PATH,
                 method: 'get',
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'application/json'
                 },
-                auth: {
-                    username: RED.settings.adminAuth.users[0].username,
-                    password: RED.settings.adminAuth.users[0].passowrd
-                }
+                timeout: 5000
             };
 
             axios.request(config).then((res) => {
-                console.log(res);
+                msg.payload = res.data;
+                node.send(msg);
                 node.status({
                     fill: "blue",
                     shape: "dot",
                     text: "success"
                 });
             }).catch((error) => {
-                console.log(error);
+                node.error(RED._("red-light.errors.response"));
                 node.status({
                     fill: "red",
                     shape: "ring",
-                    text: error.message
+                    text: RED._("red-light.errors.response")
                 });
             });
         });
     }
 
-    RED.nodes.registerType("android-light", AndroidLightNode);
+    RED.nodes.registerType("red-light", RedMobileLightNode);
 };

f80bad8a235d48157ec56df31d87191660fef062
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2019-03-23 18:12:28 +0900

    add vibrate & dialog

---
+++ nodes/beep.js
@@ -0,0 +1,56 @@
+module.exports = function(RED) {
+    'use strcit';
+
+    const axios = require('axios');
+    const qs = require('qs');
+    const BASE_URL = 'http://127.0.0.1';
+    const PATH =  '/mobile';
+
+    function RedMobileBeepNode(n) {
+        RED.nodes.createNode(this, n);
+        let node = this;
+
+        node.on('input', function(msg) {
+            if(msg.payload === undefined || typeof msg.payload !== "number"){
+                sendError(node);
+                return;
+            }
+            const json =  {
+                method: "beep",
+                payload: msg.payload
+            };
+            let config = {
+                baseURL: BASE_URL + ":" + RED.settings.redMobilePort,
+                url: PATH,
+                method: 'post',
+                data: qs.stringify(json),
+                headers: {
+                    'Authorization': "Bearer: " + RED.settings.redMobileAccessKey,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+            axios.request(config).then((res) => {
+                msg.payload = res.data;
+                node.send(msg);
+                node.status({
+                    fill: "blue",
+                    shape: "dot",
+                    text: "success"
+                });
+            }).catch((error) => {
+                sendError(node);
+            });
+        });
+
+        function sendError(node){
+            node.error(RED._("beep.errors.response"));
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: RED._("beep.errors.response")
+            });
+        }
+    }
+    RED.nodes.registerType("beep", RedMobileBeepNode);
+};
---
+++ nodes/prompt.js
@@ -0,0 +1,57 @@
+module.exports = function(RED) {
+    'use strcit';
+
+    const axios = require('axios');
+    const qs = require('qs');
+    const BASE_URL = 'http://127.0.0.1';
+    const PATH =  '/mobile';
+
+    function RedMobilePromptNode(n) {
+        RED.nodes.createNode(this, n);
+        let node = this;
+
+        node.on('input', function(msg) {
+            if(msg.payload === undefined || typeof msg.payload !== "object"){
+                sendError(node);
+                return;
+            }
+            const json =  {
+                method: "prompt",
+                payload: msg.payload
+            };
+            let config = {
+                baseURL: BASE_URL + ":" + RED.settings.redMobilePort,
+                url: PATH,
+                method: 'post',
+                data: qs.stringify(json),
+                headers: {
+                    'Authorization': "Bearer: " + RED.settings.redMobileAccessKey,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+            axios.request(config).then((res) => {
+                msg.payload = res.data;
+                node.send(msg);
+                node.status({
+                    fill: "blue",
+                    shape: "dot",
+                    text: "success"
+                });
+            }).catch((error) => {
+                sendError(node);
+            });
+        });
+    }
+
+    function sendError(node){
+        node.error(RED._("beep.errors.response"));
+        node.status({
+            fill: "red",
+            shape: "ring",
+            text: RED._("beep.errors.response")
+        });
+    }
+
+    RED.nodes.registerType("prompt", RedMobilePromptNode);
+};

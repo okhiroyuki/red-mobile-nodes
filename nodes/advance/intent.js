0762bf6f3c577e163c5f63e40b2169018038044d
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2019-08-24 14:53:06 +0900

    add intent node

---
+++ nodes/advance/intent.js
@@ -0,0 +1,53 @@
+module.exports = function(RED) {
+    'use strcit';
+
+    const axios = require('axios');
+    const qs = require('qs');
+    const BASE_URL = 'http://127.0.0.1';
+    const PATH =  '/mobile';
+
+    function sendError(node, message){
+        node.error(message);
+        node.status({
+            fill: "red",
+            shape: "ring",
+            text: message
+        });
+    }
+
+    function RedMobileStartActivityNode(n) {
+        RED.nodes.createNode(this, n);
+        let node = this;
+
+        node.on('input', function(msg) {
+            const json =  {
+                method: "intent-start-activity",
+                payload: msg.payload
+            };
+            let config = {
+                baseURL: BASE_URL + ":" + RED.settings.redMobilePort,
+                url: PATH,
+                method: "post",
+                data: qs.stringify(json),
+                headers: {
+                    'Authorization': "Bearer: " + RED.settings.redMobileAccessKey,
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            };
+
+            axios.request(config).then((res) => {
+                msg.payload = res.data;
+                node.send(msg);
+                node.status({
+                    fill: "blue",
+                    shape: "dot",
+                    text: "success"
+                });
+            }).catch((error) => {
+                sendError(node, RED._("start-activity.errors.response"));
+            });
+        });
+    }
+
+    RED.nodes.registerType("start-activity", RedMobileStartActivityNode);
+};

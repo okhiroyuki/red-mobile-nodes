330760e29192bd7abb518a6a3b6cdcb2cea19e7b
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2020-01-29 22:13:11 +0900

    add new methods
    - write
    - read

--- nodes/ble/ble.js
+++ nodes/ble/ble.js
@@ -95,6 +95,83 @@ module.exports = function(RED) {
 
     RED.nodes.registerType("ble disconnect", RedMobileBleDisconnectNode);
 
+    function RedMobileBleWriteNode(n) {
+        RED.nodes.createNode(this, n);
+        let node = this;
+        node.opts = {};
+        if (RED.nodes.getNode(n.device)){
+            node.opts.deviceId = RED.nodes.getNode(n.device).deviceId;
+            node.opts.service_uuid = RED.nodes.getNode(n.device).serviceUuid;
+            node.opts.characteristic_uuid = RED.nodes.getNode(n.device).characteristicUuid;    
+        }
+        node.opts.response = n.response;
+
+        node.on('input', function(msg) {
+            const json =  {
+                method: "ble-write",
+                payload: msg.payload,
+                opts: node.opts
+            };
+
+            axios.request(getPostConfig(json)).then((res) => {
+                msg.payload = res.data;
+                node.send(msg);
+                node.status({
+                    fill: "blue",
+                    shape: "dot",
+                    text: "success"
+                });
+            }).catch((error) => {
+                node.error(error.message);
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: error.message
+                });
+            });
+        });
+    }
+
+    RED.nodes.registerType("ble write", RedMobileBleWriteNode);
+
+    function RedMobileBleReadNode(n) {
+        RED.nodes.createNode(this, n);
+        let node = this;
+        node.opts = {};
+        if (RED.nodes.getNode(n.device)){
+            node.opts.deviceId = RED.nodes.getNode(n.device).deviceId;
+            node.opts.service_uuid = RED.nodes.getNode(n.device).serviceUuid;
+            node.opts.characteristic_uuid = RED.nodes.getNode(n.device).characteristicUuid;    
+        }
+
+        node.on('input', function(msg) {
+            const json =  {
+                method: "ble-read",
+                payload: msg.payload,
+                opts: node.opts
+            };
+
+            axios.request(getPostConfig(json)).then((res) => {
+                msg.payload = res.data;
+                node.send(msg);
+                node.status({
+                    fill: "blue",
+                    shape: "dot",
+                    text: "success"
+                });
+            }).catch((error) => {
+                node.error(error.message);
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: error.message
+                });
+            });
+        });
+    }
+
+    RED.nodes.registerType("ble read", RedMobileBleReadNode);
+
     function RedMobileBleScanNode(n) {
         RED.nodes.createNode(this, n);
         let node = this;
@@ -133,77 +210,9 @@ module.exports = function(RED) {
     function bleDevice(n){
         RED.nodes.createNode(this, n);
         this.deviceId = n.deviceId;
+        this.serviceUuid = n.serviceUuid;
+        this.characteristicUuid = n.characteristicUuid;
     }
     RED.nodes.registerType("bledevice", bleDevice);
 
-    // function RedMobileSerialWriteNode(n) {
-    //     RED.nodes.createNode(this, n);
-    //     let node = this;
-    //     node.data = n.data;
-    //     node.dataType = n.dataType;
-
-    //     node.on('input', function(msg) {
-    //         const json =  {
-    //             method: "serial-write",
-    //             payload: node.data,
-    //             dataType: node.dataType
-    //         };
-    //         if(!node.data){
-    //             if(msg.payload){
-    //                 json.payload = msg.payload;
-    //             }else{
-    //                 node.error(RED._("serial-write.errors.payload"));
-    //                 node.status({
-    //                     fill: "red",
-    //                     shape: "ring",
-    //                     text: RED._("serial-write.errors.payload")
-    //                 });
-    //                 return;
-    //             }
-    //         }
-
-    //         axios.request(getPostConfig(json)).then((res) => {
-    //             msg.payload = res.data;
-    //             node.send(msg);
-    //             node.status({
-    //                 fill: "blue",
-    //                 shape: "dot",
-    //                 text: "success"
-    //             });
-    //         }).catch((error) => {
-    //             node.error(RED._("serial-write.errors.response"));
-    //             node.status({
-    //                 fill: "red",
-    //                 shape: "ring",
-    //                 text: RED._("serial-write.errors.response")
-    //             });
-    //         });
-    //     });
-    // }
-
-    // RED.nodes.registerType("serial-write", RedMobileSerialWriteNode);
-
-    // function RedMobileSerialReadNode(n) {
-    //     RED.nodes.createNode(this, n);
-    //     let node = this;
-    //     const ev = new EventEmitter();
-    //     const ws = new WebSocketClient(ev);
-
-    //     ws.open("ws://localhost:" + RED.settings.redMobileWsPort + "/mobile/serial");
-    //     ev.on("open",() => {
-    //         node.status({fill: "blue",shape: "dot",text: "connect"});
-    //     });
-    //     ev.on("close", () => {
-    //         node.status({fill: "green",shape: "dot",text: "close"});
-    //     });
-    //     ev.on("error", (e)=>{
-    //         node.status({fill: "red",shape: "dot",text: "error"});
-    //         node.send({payload:e});
-    //     });
-    //     ev.on("message" ,(data)=>{
-    //         node.send({payload: JSON.parse(data)});
-    //     });
-    // }
-
-    // RED.nodes.registerType("serial-read", RedMobileSerialReadNode);
 };

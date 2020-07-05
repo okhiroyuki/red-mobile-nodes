8b9f46ef9046a7907a37cba484a8078ad6b78989
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2020-07-05 10:05:08 +0900

    add lib for fire

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,7 @@ module.exports = function(grunt){
         copy: {
             fire: {
               files: [
-                {expand: true, cwd: './lib/', src: ['**'], dest: './packages/android/lib/'},
+                {expand: true, cwd: './lib/', src: ['**'], dest: './packages/fire/lib/'},
                 {expand: true, cwd: './nodes/cognitives/text-to-speech/', src: ['**'], dest: './packages/fire/nodes/cognitives/text-to-speech/'},
                 {expand: true, cwd: './nodes/in-app-browser/', src: ['**'], dest: './packages/fire/nodes/in-app-browser/'},
                 {expand: true, cwd: './nodes/intent/', src: ['**'], dest: './packages/fire/nodes/intent/'},

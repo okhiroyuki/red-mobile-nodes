fcc881c85d348760ce43053415ff8b3faca85d55
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-02-12 10:21:52 +0900

    feat: update inAppBrowser

--- nodes/in-app-browser/in-app-browser.js
+++ nodes/in-app-browser/in-app-browser.js
@@ -7,12 +7,15 @@ module.exports = function(RED) {
     function RedMobileInAppBrowserNode(n) {
         RED.nodes.createNode(this, n);
         let node = this;
+        node.target = n.target;
 
         node.on('input', function(msg) {
             const json =  {
                 id: node.id,
                 method: "in-app-browser",
-                payload: msg.payload
+                payload: msg.payload,
+                target: node.target,
+                options: msg.options,
             };
             util.postRequest(node, msg, json);
         });

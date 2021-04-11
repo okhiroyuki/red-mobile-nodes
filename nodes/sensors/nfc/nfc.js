dc9bf4ad2e01386eb884793da04b13fa8d5eaf3b
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2021-04-11 16:17:38 +0900

    feat: add nfc node

---
+++ nodes/sensors/nfc/nfc.js
@@ -0,0 +1,24 @@
+module.exports = function(RED) {
+    'use strcit';
+
+    const EventEmitter = require('events').EventEmitter;
+    const WebSocketClient = require('../../WebSocketClient');
+    const ev = new EventEmitter();
+    const ws = new WebSocketClient(ev);
+    if(RED.settings.redMobileWsPort){
+        const port = RED.settings.redMobileWsPort;
+        ws.open("ws://localhost:" + port + "/mobile/nfc");
+    }
+
+    function RedMobileNfcNode(n) {
+        RED.nodes.createNode(this, n);
+        let node = this;
+
+        ev.on("message" ,(data) => {
+            const payload = JSON.parse(data).payload;
+            node.send({"payload": payload});    
+        });
+    }
+
+    RED.nodes.registerType("nfc", RedMobileNfcNode);
+};
